7a6b0612f2b5d62e187aabaacb4c38e6f17a1c65
Paola Stephanie <paolaa.seb@gmail.com>
2026-05-19 21:58:12 -0300

    Atualizando aula04 (principlamente Perfil)

--- aulas/aula04/src/contexts/AuthContext.jsx
+++ aulas/aula04/src/contexts/AuthContext.jsx
@@ -6,7 +6,7 @@ const AuthContext = createContext();
 
 // cria o provedor
 function AuthProvider({ children }) {
-    const [logado, setLogado] = useState(false);
+    const [logado, setLogado] = useState(true);
     const [usuario, setUsuario] = useState({});
 
     // chamar a API passando dados
--- aulas/aula04/src/pages/Perfil.jsx
+++ aulas/aula04/src/pages/Perfil.jsx
@@ -1,13 +1,93 @@
 import { useParams } from "react-router";
-
-
+import { useForm } from "react-hook-form";
 
 function Perfil() {
-    const { id } = useParams();
+  const { id } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // register() => {name="" onChange="" onBlur="" }
+  // handleSubmit() => dispara a validação; informa ao React que mudou os campos
 
-    // buscar os dados do usuário
+  const salvar = (data) => console.log(data);
 
-    return <h1>Perfil do Usuário {id} </h1>;
+  // buscar os dados do usuário
+
+  const regras = {
+    // aqui são os obrigatórios com "required"
+    nome: { 
+        required: "Nome é obrigatório",
+        minLength: { value: 3, message: "Nome tem que ter mais de 2 caracteres"},
+        maxLength: { value: 100, message: "Nome tem no máximo 100 caracteres"}
+    },
+    email: { 
+        required: "Email é obrigatório",
+        pattern: { 
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: "Email inválido",
+        },
+    },
+    nascimento: {
+        validate: { 
+            dataMinima: (value) => 
+                Date.parse(value) > new Date(1899, 11, 31).getTime() || "Data inferior",
+            dataMaxima: (value) => 
+                Date.parse(value) < new Date().getTime() || "Data superior",
+        },
+    },
+    telefone: {
+        min:  { value: 0, message: "Telefone inválido"},
+        pattern: { 
+            value: /^(\(?\d{2}\)?[\s.-]?)?(\d{4,5})[\s.-]?(\d{4})$/, 
+            message: "Telefone inválido"
+        },
+    },
+  };
+
+  return (
+    <>
+      <h1>Perfil do Usuário {id} </h1>
+      <form onSubmit={handleSubmit(salvar)}>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input 
+            type="text" 
+            id="nome" {...register("nome", regras.nome)} 
+          />
+            {errors?.nome && <p>{errors.nome.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input 
+            type="email" 
+            id="email" {...register("email", regras.email)} 
+          />
+            {errors?.email && <p>{errors.email.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="nascimento">Data de Nascimento</label>
+          <input
+            type="date"
+            id="nascimento"
+            {...register("nascimento", regras.nascimento)}
+          />
+            {errors?.nascimento && <p>{errors.nascimento.message}</p>}
+        </div>
+        <div>
+          <label htmlFor="telefone">Telefone</label>
+          <input
+            type="tel"
+            id="telefone"
+            {...register("telefone", regras.telefone)}
+          />
+            {errors?.telefone && <p>{errors.telefone.message}</p>}
+        </div>
+        <button type="submit">Salvar</button>
+      </form>
+    </>
+  );
 }
 
 export default Perfil;
